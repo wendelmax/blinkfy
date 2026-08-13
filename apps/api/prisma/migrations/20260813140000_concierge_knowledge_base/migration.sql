5bf0a36a19de5e411d4ff44f5d2a5e74653657ad
Jackson Sá <jacksonwendel@gmail.com>
2026-08-13 15:26:50 -0300

    fix: reference legacy Client table in knowledge migration

--- apps/api/prisma/migrations/20260813140000_concierge_knowledge_base/migration.sql
+++ apps/api/prisma/migrations/20260813140000_concierge_knowledge_base/migration.sql
@@ -17,5 +17,5 @@ CREATE TABLE "knowledge_chunks" (
 CREATE UNIQUE INDEX "knowledge_documents_clientId_title_key" ON "knowledge_documents"("clientId", "title");
 CREATE INDEX "knowledge_documents_clientId_createdAt_idx" ON "knowledge_documents"("clientId", "createdAt");
 CREATE UNIQUE INDEX "knowledge_chunks_documentId_position_key" ON "knowledge_chunks"("documentId", "position");
-ALTER TABLE "knowledge_documents" ADD CONSTRAINT "knowledge_documents_clientId_fkey" FOREIGN KEY ("clientId") REFERENCES "clients"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "knowledge_documents" ADD CONSTRAINT "knowledge_documents_clientId_fkey" FOREIGN KEY ("clientId") REFERENCES "Client"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 ALTER TABLE "knowledge_chunks" ADD CONSTRAINT "knowledge_chunks_documentId_fkey" FOREIGN KEY ("documentId") REFERENCES "knowledge_documents"("id") ON DELETE CASCADE ON UPDATE CASCADE;
